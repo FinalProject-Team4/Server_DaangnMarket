a7d6bc7dfc516b7552cc7facc26cdcbc064b7568
znfod-dev <znfod.dev@gmail.com>
2020-03-27 13:20:08 +0900

    change page size

--- app/config/settings.py
+++ app/config/settings.py
@@ -121,6 +121,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 10,
 }
 
