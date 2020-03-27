aaabc34a43c48189f0fa143dce23b34b7ea64ba9
znfod-dev <znfod.dev@gmail.com>
2020-03-27 13:19:12 +0900

    update page

--- app/config/settings.py
+++ app/config/settings.py
@@ -118,3 +118,9 @@ STATIC_ROOT = os.path.join(ROOT_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+}
+
