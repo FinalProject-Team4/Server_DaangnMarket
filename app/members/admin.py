d6224136bae9c0998d70cfe3ff92ad89ba35780d
jam <nevvjann@gmail.com>
2020-04-06 15:52:41 +0900

    fix: User model admin read_only

--- app/members/admin.py
+++ app/members/admin.py
@@ -4,5 +4,8 @@ from .models import User
 
 @admin.register(User)
 class MembersAdmin(admin.ModelAdmin):
-    list_display = ['username', 'phone', 'avatar', 'created', 'updated']
+    list_display = ['username', 'phone', 'avatar']
     list_filter = ['username', 'created', 'updated']
+    search_fields = ['username', 'phone']
+    fields = ('uid', 'username', 'phone', 'avatar','created', 'updated')
+    readonly_fields = ("created", 'updated',)
