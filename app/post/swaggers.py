a75f1aab785d7c8a16ef3154fdafe9c641915109
jam <nevvjann@gmail.com>
2020-04-14 00:32:30 +0900

    chore: edit swagger

--- app/post/swaggers.py
+++ app/post/swaggers.py
@@ -2,14 +2,11 @@ from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
 from core.swagger_custom import ExampleInspector
-from post.serializers import PostImageUploadSerializer
 from post.views import *
 
 # 상품 이미지 업로드
 decorated_post_image_upload_api = \
     swagger_auto_schema(
-        # operation_description="게시물 업로드 API",
-        method='post',
         request_body=openapi.Schema(
             type=openapi.TYPE_OBJECT,
             required=['post_id', 'photos'],
@@ -41,4 +38,10 @@ decorated_post_image_upload_api = \
         field_inspectors=[ExampleInspector],
         security=[],
         tags=['post'],
-    )(ApiPostImageUpload.as_view())
+    )
+
+# 게시물 생성
+decorated_post_create_api = \
+    swagger_auto_schema(
+        consumes='multipart/form-data',
+    )
