f6cb1f4e8eab649a8471659c06a21ba7d7f20003
jam <nevvjann@gmail.com>
2020-04-15 21:11:24 +0900

    fix: typo

--- app/post/views.py
+++ app/post/views.py
@@ -191,7 +191,7 @@ class ApiPostCreateLocate(CreateAPIView):
 
 # post edit
 
-# @method_decorator(name='post', decorator=decorated_post_image_upload_api)
+@method_decorator(name='post', decorator=decorated_post_image_upload_api)
 class ApiPostImageUpload(CreateAPIView):
     """
     상품 이미지 업로드
