5a2d27f37aafc284dea4f0b73e00ce845706059d
znfod-dev <znfod.dev@gmail.com>
2020-04-20 14:46:29 +0900

    update notification

--- app/notification/serializers.py
+++ app/notification/serializers.py
@@ -1,3 +1,4 @@
+from push_notifications.api.rest_framework import GCMDeviceSerializer
 from rest_framework import serializers
 
 from notification.models import Notification
@@ -5,8 +6,12 @@ from notification.models import Notification
 
 class NotificationSerializer(serializers.ModelSerializer):
 
+    sender = serializers.CharField(source='sender.name')
+    receiver = serializers.CharField(source='receiver.name')
+
     class Meta:
         model = Notification
+        depth = 0
         fields = (
-            'title', 'body',
+            'sender', 'receiver', 'title', 'body',
         )
--- app/notification/views.py
+++ app/notification/views.py
@@ -4,7 +4,7 @@ from push_notifications.models import GCMDevice
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -12,6 +12,7 @@ from members.models import User
 
 
 # device 등록
+from notification.models import Notification
 from notification.serializers import NotificationSerializer
 
 
@@ -45,16 +46,57 @@ class ApiSendFcm(CreateAPIView):
         user = self.request.user
 
         sender = GCMDevice.objects.get(name=user.username)
-        receiver = GCMDevice.objects.get(user=user.username)
+        receiver = GCMDevice.objects.get(name=user.username)
         serializer = self.get_serializer(data=request.data)
 
         if serializer.is_valid():
             notification = serializer.save(sender=sender, receiver=receiver)
             receiver.send_message(notification.body, extra={"title": notification.title})
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response(status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+# 알림 발송
+class ApiSendNotice(CreateAPIView):
+    serializer_class = NotificationSerializer
+    authentication_classes = [TokenAuthentication]
 
+    def create(self, request, *args, **kwargs):
+        user = self.request.user
+        if not user.is_staff:
+            return Response('this user is not admin', status=status.HTTP_401_UNAUTHORIZED)
+        sender = GCMDevice.objects.get(name='관리자')
+
+        receivers = GCMDevice.objects.all().exclude(name='관리자')
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            for receiver in receivers:
+                notification = serializer.save(sender=sender, receiver=receiver)
+                if notification.title == '':
+                    receiver.send_message(notification.body)
+                else:
+                    receiver.send_message(notification.body, extra={"title": notification.title})
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# 알림 리스트 가져오기
+class ApiListNotice(ListAPIView):
+    serializer_class = NotificationSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        receiver = GCMDevice.objects.get(name=user.username)
+        notifications = Notification.objects.filter(receiver=receiver)
+        return notifications
+
+
+# 알림 삭제
+class ApiDeleteNotice(DestroyAPIView):
+
+    def destroy(self, request, *args, **kwargs):
+        pass
 
